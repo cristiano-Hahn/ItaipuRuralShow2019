db1e3761d7a4735c22aab9c80863c93f8202e562
Cristiano Hahn <cristianohahn.ch@gmail.com>
2018-03-25 10:36:01 -0300

    First blood!

---
+++ app/src/main/java/com/example/cristianohahn/itaipururalshow2019/SplashScreenActivity.java
@@ -0,0 +1,31 @@
+package com.example.cristianohahn.itaipururalshow2019;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.WindowManager;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getBaseContext(), MainActivity.class));
+            }
+        }, 5000);
+    }
+
+}
